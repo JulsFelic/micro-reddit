1be95ce249c054d4fc20d880af19f4d40e18011f
Julian Feliciano <julsfelic@gmail.com>
2015-02-26 14:59:40 -0600

    Finish User model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  before_save { email.downcase! }
+  validates :username, length: { minimum: 4, maximum: 30 },
+                       presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
+end
